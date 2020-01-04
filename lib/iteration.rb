055919760221c2afbf22d06fabbbcc8f0e2ac416
Paul Hoffart <pahoffart25@gmail.com>
2020-01-04 03:21:51 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,13 +1,15 @@
 def join_ingredients(src)
+  pizza = ["I love pizza"]
   row_index = 0
 while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
-    return src[row_index][element_index]
+     src[row_index][element_index]
     element_index += 1
   end
   row_index += 1
 end
+return pizza
 end
 
 def find_greater_pair(src)
